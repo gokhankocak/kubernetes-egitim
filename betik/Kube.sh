854340ca3482a21bbf397b6a1fc3edc15b53e285
Gökhan Koçak <gokhan.kocak@mail.ru>
2020-06-14 04:51:27 +0300

    Create Kube.sh

---
+++ betik/Kube.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# MIT License
+# Copyright (c) 2020 Gökhan Koçak
+# www.gokhankocak.com
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo snap install microk8s --classic
+sudo microk8s status --wait-ready
+sudo microk8s enable dashboard dns registry
+sudo microk8s kubectl get all --all-namespaces
+sudo usermod -a -G microk8s ubuntu
+sudo chown -f -R ubuntu ~/.kube
+sudo microk8s status
